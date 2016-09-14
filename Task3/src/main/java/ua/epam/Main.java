0fe071918f20d738560f70502e778f46efa9ba8a
Volodymyr Zaverukha <volodymyr_zaverukha@epam.com>
2016-09-14 19:00:51 +0300

    Tested method uncommented

--- Task3/src/main/java/ua/epam/Main.java
+++ Task3/src/main/java/ua/epam/Main.java
@@ -9,18 +9,18 @@ public class Main {
     }
 
     public static int[] getInts(int[] s, int k) {
-//        int[] res = new int[s.length];
-//        int j = 0;
-//        for (int i = 0; i < s.length; i++){
-//            if (s[i] == k){
-//                res[j] = i;
-//                j++;
-//            }
-//        }
-//        int [] result = new int[j];
-//        System.arraycopy(res, 0, result, 0, j);
-//        return result;
-        return null;
+        int[] res = new int[s.length];
+        int j = 0;
+        for (int i = 0; i < s.length; i++){
+            if (s[i] == k){
+                res[j] = i;
+                j++;
+            }
+        }
+        int [] result = new int[j];
+        System.arraycopy(res, 0, result, 0, j);
+        return result;
+        
     }
 }
 
